bf463d8a495594f68334d7db8a1369dd630a6f8a
OmprakashKadur <kaduromee@gmail.com>
2025-09-26 09:35:59 +0530

    graceful shutdown

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,8 +1,61 @@
 package main
+
 import (
+	"context"
 	"fmt"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/OmprakashKadur/students-api/internal/config"
 )
 
-func main(){
+func main() {
 	fmt.Println("Hello, Students API")
+	//load config
+	cfg := config.MustLoad()
+
+	//database setup
+
+	//setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students API"))
+	})
+
+	//setup server
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	slog.Info("server started", slog.String("address", cfg.Addr))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to Start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+	slog.Info("server shutdown successfully")
 }
